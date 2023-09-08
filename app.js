0bc04697db3c09e440ecabe7a48172213a828f76
Thiago <kirito98.1234@gmail.com>
2023-09-08 17:00:11 -0300

    ponemos casi todos los archivos

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const puerto = 3000;
+const path = require('path');
+const bodyParser = require('body-parser');
+const dbConnection = require('./mongodbconnection/index.js');
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'publico')));
+
+
+const routes = require('./routes');
+app.use('/', routes);
+
+dbConnection.connect(); // Conecta a la base de datos
+
+app.listen(puerto, () => {
+  console.log(`Servidor escuchando en el puerto ${puerto}`);
+});
+
