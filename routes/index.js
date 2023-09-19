81f599314c57af6a74390af4a750030cb7a1d9c7
nahuelonnn <57152858+nahuelonnn@users.noreply.github.com>
2023-09-19 12:15:39 -0300

    commit

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const { saveSequency, getAllSequences } = require("../service/index");
 
 router.get("/", async (req, res) => {
 	const sequences = await getAllSequences();
+	console.log(sequences)
 	res.render("index.ejs", { sequences });
 });
 
@@ -16,8 +17,9 @@ router.get("/Conocenos", async (req, res) => {
 	res.render("Conocenos.ejs");
 });
 
-router.get("/inicio", async (req, res) => {
-	res.render("inicio.ejs");
+router.get("/proteinas", async (req, res) => {
+	const sequences = await getAllSequences();
+	res.render("proteinas.ejs", { sequences });
 });
 
 router.post("/", async (req, res) => {
