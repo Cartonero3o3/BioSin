c037f2ad59e7cf301fe6eef0927c3008341272a4
Tmucho0 <williamstomas2610@gmail.com>
2023-09-23 19:49:54 -0300

    intentamos hacer andar vercel

--- app.js
+++ app.js
@@ -9,15 +9,26 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Servir archivos estáticos desde el directorio 'publico'
 app.use(express.static(path.join(__dirname, 'publico')));
 
+// Conexión a la base de datos
+dbConnection.connect();
 
-const routes = require('./routes');
-app.use('/', routes);
-
-dbConnection.connect(); // Conecta a la base de datos
+// Middleware para manejar la Directory Listing
+app.use((req, res, next) => {
+  const dirPath = path.join(__dirname, 'publico', req.path);
+  
+  // Comprobar si la ruta es un directorio
+  if (req.path.endsWith('/') || req.path === '/') {
+    // Listar el contenido del directorio
+    return res.sendFile(path.join(dirPath, 'index.html'));
+  }
+  
+  // Si no es un directorio, continuar con el siguiente middleware
+  next();
+});
 
 app.listen(puerto, () => {
   console.log(`Servidor escuchando en el puerto ${puerto}`);
 });
-
